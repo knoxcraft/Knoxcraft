a4c08b166cdb5ba617f74efb2096fed17895bdc7
Jaime Spacco <jspacco@knox.edu>
2015-08-21 14:48:02 -0500

    Tried adding hooks and gamerules for weather daylight from code.
    However, it did not seem to work. Will need to try something else.

--- pykc/test-pykc.py
+++ pykc/test-pykc.py
@@ -1,15 +1,18 @@
 from pykc import Turtle, BlockType as bt
 
 t = Turtle('francois')
-t.forward(10)
-t.turnRight(90)
+
 t.setBlock(bt.BlueWool)
-t.forward(10)
-t.turnRight(90)
-t.setBlock(bt.GreenWool)
-t.forward(10)
-t.turnRight(90)
-t.setBlock(bt.Dirt)
-t.forward(10)
+size=6
+for j in range(size):
+	for i in range(size):
+		t.blockPlace(True)
+		t.forward(size)
+		t.blockPlace(False)
+		t.right(1)
+		t.backward(size)
+	t.blockPlace(False)
+	t.left(size)
+	t.up(1)
 #print(t.toJson())
 t.upload('127.0.0.1:8888', 'Spacdog')
