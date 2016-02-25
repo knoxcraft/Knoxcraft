fbe382e1022fe9fa2c78842a22269ce93828b019
Emily Hastings <emhastings@knox.edu>
2016-02-24 22:43:12 -0600

    Finished code generators
    This commit updates the code generators to generate Javascript which
    generates JSON for the script.  The next step is to add a working submit
    button.

--- blockly/generators/javascript/turtle.js
+++ blockly/generators/javascript/turtle.js
@@ -2,28 +2,31 @@
 *  Code generators for Knoxcraft Turtles
 */
 
-//TODO:  Do we want these to just produce json, or to be print statments and then run the code?  (for loops, ifs, etc?)
-//TODO:  Update:  yes.  We need to print and run the code, because otherwise the loops mess up the json.
-
 Blockly.JavaScript['turtle_init'] = function(block) {
   var text_name = block.getFieldValue('name');
   var statements_script = Blockly.JavaScript.statementToCode(block, 'script');
+			
+  var code = 'var json = \'\';\n';
+  code += 'json += \'{\\n     "scriptname" : "' + text_name + '",\\n     "commands" : [\\n\';';
+  code += statements_script; 
   
   //remove trailing comma
-  if (statements_script.endsWith(',\n'))  {
-	  statements_script = statements_script.substring(0, statements_script.length-2) + '\n';
-  }
+  code += 'if (json.endsWith(\',\\n\'))  {';
+  code += '     json = json.substring(0, json.length-2) + \'\\n\';\n';
+  code += '}';
   
-  var code = '{ \n' + Blockly.JavaScript.INDENT + '"scriptname" : "' + text_name + 
-			'",\n' + Blockly.JavaScript.INDENT + '"commands" : [\n' + statements_script + Blockly.JavaScript.INDENT + ']\n}';
-  
+  code += 'json += \'     ]\\n}\';\n';
+	
+  code += 'alert(json);\n'; //display for testing
+			
   return code;
 };
 
 Blockly.JavaScript['turtle_move'] = function(block) {
   var text_dist = block.getFieldValue('dist');
   var dropdown_dir = block.getFieldValue('dir');
-  var code = Blockly.JavaScript.INDENT + '{"cmd" : "' + dropdown_dir.toLowerCase() + '", \n' + Blockly.JavaScript.INDENT +  Blockly.JavaScript.INDENT + '"args" : {"dist" : ' + text_dist + '}},\n';
+  var code = 'json += \'     {"cmd" : "' + dropdown_dir.toLowerCase() + '", \\n          "args" : {"dist" : ' + text_dist + '}},\\n\';\n';
+  
   return code;
 };
 
@@ -32,28 +35,28 @@ Blockly.JavaScript['turtle_turn'] = function(block) {
   var dropdown_turndir = block.getFieldValue('turnDir');
   var code = '';
   if (dropdown_turndir == 'LEFT')  {
-	code = Blockly.JavaScript.INDENT + '{"cmd" : "turnLeft", \n' + Blockly.JavaScript.INDENT + Blockly.JavaScript.INDENT + '"args" : {"degrees" : ' + text_deg + '}},\n';
+	code = 'json += \'     {"cmd" : "turnLeft", \\n          "args" : {"degrees" : ' + text_deg + '}},\\n\';\n';
   }  else {
-	code = Blockly.JavaScript.INDENT + '{"cmd" : "turnRight", \n' + Blockly.JavaScript.INDENT + Blockly.JavaScript.INDENT + '"args" : {"degrees" : ' + text_deg + '}},\n';
+	code = 'json += \'     {"cmd" : "turnRight", \\n          "args" : {"degrees" : ' + text_deg + '}},\\n\';\n';
   }
   return code;
 };
 
 Blockly.JavaScript['turtle_setblockplace'] = function(block) {
   var dropdown_mode = block.getFieldValue('mode');
-  var code = Blockly.JavaScript.INDENT + '{"cmd" : "setBlockPlace", \n' + Blockly.JavaScript.INDENT + Blockly.JavaScript.INDENT + '"args" : {"mode" : "' + dropdown_mode.toLowerCase() + '"}},\n';
+  var code = 'json += \'     {"cmd" : "setBlockPlace", \\n          "args" : {"mode" : "' + dropdown_mode.toLowerCase() + '"}},\\n\';\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_setblocktype'] = function(block) {
   var text_type = block.getFieldValue('type');
-  var code = Blockly.JavaScript.INDENT + '{"cmd" : "setBlock", \n' + Blockly.JavaScript.INDENT + Blockly.JavaScript.INDENT + '"args" : {"type" : ' + text_type + '}},\n';
+  var code = 'json += \'     {"cmd" : "setBlock", \\n          "args" : {"type" : ' + text_type + '}},\\n\';\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_setblocktype2'] = function(block) {
   var dropdown_type = block.getFieldValue('type');
-  var code = Blockly.JavaScript.INDENT + '{"cmd" : "setBlock", \n' + Blockly.JavaScript.INDENT + Blockly.JavaScript.INDENT + '"args" : {"type" : ' + dropdown_type + '}},\n';
+  var code = 'json += \'     {"cmd" : "setBlock", \\n          "args" : {"type" : ' + dropdown_type + '}},\\n\';\n';
   return code;
 };
 
@@ -61,13 +64,13 @@ Blockly.JavaScript['turtle_setpos'] = function(block) {
   var text_x = block.getFieldValue('X');
   var text_y = block.getFieldValue('Y');
   var text_z = block.getFieldValue('Z');
-  var code = Blockly.JavaScript.INDENT + '{"cmd" : "setPosition", \n' + Blockly.JavaScript.INDENT + Blockly.JavaScript.INDENT + '"args" : {"x" : ' + text_x + ', "y" : ' + text_y + ', "z": ' + text_z + '}},\n';
+  var code = 'json += \'     {"cmd" : "setPosition", \\n          "args" : {"x" : ' + text_x + ', "y" : ' + text_y + ', "z": ' + text_z + '}},\\n\';\n';
   return code;
 };
 
 Blockly.JavaScript['turtle_setdir'] = function(block) {
   var dropdown_dir = block.getFieldValue('dir');
-  // TODO: Is this the right json for directions?
-  var code = Blockly.JavaScript.INDENT + '{"cmd" : "setDirection", \n' + Blockly.JavaScript.INDENT + Blockly.JavaScript.INDENT + '"args" : {"dir" : "' + dropdown_dir + '"}},\n';
+  // TODO: Is this the right json for directions?  Or does it need to be converted to an int?  I can't find it in our code anywhere.
+  var code = 'json += \'     {"cmd" : "setDirection", \\n          "args" : {"dir" : "' + dropdown_dir + '"}},\\n\';\n';
   return code;
 };
