602afd22e3b7989f4cf3a0f3a7e32401867b5817
Jaime Spacco <jspacco@knox.edu>
2016-08-18 21:12:19 -0500

    added shell script for publishing to github pages

---
+++ knoxeljs/publish.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+LOCATION=../../knoxcraft.github.io
+OS=`uname`
+
+datestr=`date +"%Y-%m-%d-%H-%M-%S"`
+echo $datestr
+
+# on Linux, use cp -a
+CMD="cp -a"
+if [ "$OS" = "Darwin" ]; then
+    # on OSX, use ditto
+    CMD=ditto
+fi
+
+echo "browserify gameScript.js -o bundle.js"
+browserify gameScript.js -o bundle.js
+
+for f in textures images index.html ace-builds bundle.js java ; do
+    echo "updating $LOCATION/$f, if necessary"
+    $CMD $f $LOCATION/$f
+done
+
+
+pushd .
+cd $LOCATION
+git add .
+git commit -m "posting new version at $datestr"
+git push origin master
+popd
+
