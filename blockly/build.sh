ab1989d643605f1ccbac3a2ac96497bdefb1f4ba
Jaime Spacco <jspacco@knox.edu>
2016-08-18 23:54:35 -0500

    working on saving/restoring the workspace when switching tabs for Blockly; probably need a 'clear' button

--- blockly/build.sh
+++ blockly/build.sh
@@ -3,5 +3,5 @@
 cat ./blockly_compressed.js ./blocks_compressed.js \
   ./javascript_compressed.js ./msg/js/en.js \
   ./blocks/turtle.js ./generators/javascript/turtle.js \
-  ./blockly.js > blockly-bundle.js
-#minify
+  ./appengine/storage.js ./blockly.js > blockly-bundle.js
+
