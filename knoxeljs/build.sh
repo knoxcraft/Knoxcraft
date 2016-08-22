f4024034eaf19e93dbe8a21bcbbe5237b3902b1b
Jaime Spacco <jspacco@knox.edu>
2016-08-21 22:39:16 -0500

    skulpt only supports http, not https, so we have to commit libraries locally

--- knoxeljs/build.sh
+++ knoxeljs/build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# TODO: use gradle to build knoxcraft.github.io
+
 browserify gameScript.js --stand-alone knoxeljs > bundle.js
 mkdir -p pykc
+# TODO: use ditt/cp -a
 cp ../pykc/pykcbase.py pykc
+# TODO: only replace when there is a newer version on the server
+wget http://www.skulpt.org/static/skulpt.min.js -O pykc/skulpt.min.js
+wget http://www.skulpt.org/static/skulpt-stdlib.js -O pykc/skulpt-stdlib.js
 pushd .
 cd ../blockly && ./build.sh
 popd
